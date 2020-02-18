df82551e14509e691bb6957ada4a5a0358211510
mkssoo <36142170+mkssoo@users.noreply.github.com>
2020-02-18 12:51:09 +0900

    add \n

--- 20200217/main.go
+++ 20200217/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("received request")
-		fmt.Fprintf(w, "Hello Docker!!")
+		fmt.Fprintf(w, "Hello Docker!!\n")
 	})
 
 	log.Println("start server")
